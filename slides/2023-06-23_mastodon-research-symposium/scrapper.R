89ac7ee8f63cb2f7dee401c4bc26fe62208736ee
Carlos Camara <Carlos.Camara@warwick.ac.uk>
2023-10-31 22:38:56 +0000

    Add slides

---
+++ slides/2023-06-23_mastodon-research-symposium/scrapper.R
@@ -0,0 +1,21 @@
+library(httr)
+library(jsonlite)
+
+
+# FediDB ------------------------------------------------------------------
+
+# FediDB uses a crawler to get info from known servers. The process is somewhat
+# documented here: https://fedidb.org/crawler.html
+# API Documentation: https://fedidb.org/docs/api/v1
+
+res_software <- GET("https://api.fedidb.org/v1/software?limit=40")
+res_servers <- GET("https://api.fedidb.org/v1/servers?limit=40")
+
+df_software <- as.data.frame(fromJSON(rawToChar(res_software$content)))
+df_software$date_scraped <- Sys.Date()
+
+df_servers <- as.data.frame(fromJSON(rawToChar(res_servers$content))$data)
+df_servers$date_scraped <- Sys.Date()
+
+write.csv(df_software, "slides/2023-06-23_mastodon-research-symposium/data/fedidb_software.csv", row.names = FALSE)
+write.csv(df_servers, "slides/2023-06-23_mastodon-research-symposium/data/fedidb_servers.csv", row.names = FALSE)
